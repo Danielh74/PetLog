e49880b41c85a7c755e90bcafc8da8b643e5c4d2
Danielh74 <danielh07042000@gmail.com>
2026-06-15 20:11:43 +0300

    feat: update server setup with mongoose integration and dotenv configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi');
+})
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080")
+})
